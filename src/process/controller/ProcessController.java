2636bfe0ce4bd9e4fb730309c0e791ac93b72596
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-09-07 12:16:02 -0600

    Added .gitignore File

---
+++ src/process/controller/ProcessController.java
@@ -0,0 +1,9 @@
+package process.controller;
+
+public class ProcessController
+{
+	public void start()
+	{
+		System.out.println("this is a Practice Project;)");
+	} 
+}
---
+++ src/process/controller/ProcessRunner.java
@@ -0,0 +1,14 @@
+package process.controller;
+/**
+ * 
+ * @author NOAH UFFENS
+ *
+ */
+public class ProcessRunner
+{
+	public static void main(String [ ] args)
+	{
+		ProcessController appController = new ProcessController();
+		appController.start();
+	}
+}
